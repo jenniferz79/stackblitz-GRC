4fa6aae1b023b5eaf71ac29b6722c292a7f96318
jenniferz79 <58871282+jenniferz79@users.noreply.github.com>
2023-04-27 14:35:25 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-GRC
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-yf8ehx)
---
+++ script.js
@@ -0,0 +1,15 @@
+function successCallback(token) {
+  console.log(`toke ${token}`);
+  // const URL = `https://www.google.com/recaptcha/api/siteverify?secret=6LdOZLolAAAAAP5JqDstMY2qVUJNkm4PvAeu9CR7&response=${token}`;
+
+  // async function verifyResponse() {
+  //   const result = await fetch(URL, {
+  //     method: 'POST',
+  //   });
+  //   result.json().then((data) => {
+  //     console.log(`response {result}`);
+  //   });
+  // }
+
+  // verifyResponse();
+}
